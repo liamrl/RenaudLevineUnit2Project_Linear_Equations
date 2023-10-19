6a7ab2d769582a97d3af76eff0501b1518b21465
unknown <BT_3W2_17@BKTECH.LOCAL>
2023-10-19 09:31:53 -0400

    Initial commit

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hello!\nEnter coordinate one in (x, y) format");
+        String coord1 = s.nextLine();
+        System.out.println("Enter coordinate two in (x, y) format");
+        String coord2 = s.nextLine();
+    }
+}
